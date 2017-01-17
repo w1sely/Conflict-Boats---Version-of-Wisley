2ec74bfc77948b0b7e0909375f00a2b0c51e6a49
w1sely <chen.wisley@gmail.com>
2017-01-17 16:19:39 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,19 +2,37 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Board {
+  
+  static boolean isGameOn = true;
+  static boolean hasSetupDonePlayer1 = false;
+  static boolean hasSetupDonePlayer2 = false;
 
  public static void main(String[] args) {
+  
   JFrame frame = new JFrame("Battleship");
   frame.setSize(1900, 1000);
   frame.setLayout(new GridLayout(1, 3));
-  frame.add(new LeftField());
-  frame.add(new Divider());
+ 
+  LeftField leftField = new LeftField();
+  Divider divider = new Divider(leftField);
+  
+  frame.add(leftField);
+  frame.add(divider);
   frame.add(new RightField());
   frame.setLocationRelativeTo(null);
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   frame.setVisible(true);
   frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
   
+//  while(isGameOn){
+    
+
+    
+    
+
+    
+//    isGameOn = false;
+//  }
 
  }
 
