b8c4ef92fcb76088834f5cdb012f7f0d3f370781
w1sely <chen.wisley@gmail.com>
2017-01-17 21:37:10 -0500

    Update Divider.java

--- Divider.java
+++ Divider.java
@@ -10,12 +10,20 @@ public class Divider extends JPanel {
   
   LeftField leftField;
   
+  RightField rightField;
+  
   public Divider(LeftField fieldLeft)
   {
     leftField = fieldLeft;
     init();
   }
   
+    public Divider(RightField fieldRight)
+  {
+    rightField = fieldRight;
+    init();
+  }
+
   private void init() {
     
     //Creates and stores -1 in a reference array
@@ -43,48 +51,44 @@ public class Divider extends JPanel {
     middlePanel.setLayout(new GridLayout(6 , 1));
     // ActionListener buttonListen = new ActionListener();
     for (int i = 4; i >= 0; i--){
-        boatButtons[i] = new JButton();
-        
-        //GOOD ACTION LISTENER
-    boatButtons[i].addActionListener(new ActionListener (){
-      public void actionPerformed(ActionEvent e){
-        for (int i = 0; i < 15; i++){
-      for (int j = 0 ; j < 15; j++){
-
-        leftField.jb[i][j].setEnabled(true);
-      }
-        }
-        
-        String Direction = null;
-            
-      //Gives user option to choose what direction to place their ships
-      Direction = JOptionPane.showInputDialog (null, "Please choose a direction");
-      
-     
-      
-    }
-    });
-        
+        boatButtons[i] = new JButton(); 
         boatButtons[i].setName((i+1)+"");
         boatButtons[i].setPreferredSize(new Dimension(10,10));                                
-        middlePanel.add(boatButtons[i]);
+        middlePanel.add(boatButtons[i]); 
         boatButtons[i].addActionListener(new ActionListener(){
           
           public void actionPerformed(ActionEvent event) {
-            
-            
+                     
             System.out.println(((JButton)event.getSource()).getName());
             size[0] = ((JButton)event.getSource()).getName();
             
           }
           
         });
+        
+        //GOOD ACTION LISTENER
+        boatButtons[i].addActionListener(new ActionListener (){
+          public void actionPerformed(ActionEvent e){
+            for (int i = 0; i < 15; i++){
+              for (int j = 0 ; j < 15; j++){
+                leftField.jb[i][j].setEnabled(true);  
+              }
+            }//end of for loops
+            
+            boolean boatClicked=true;
+            
+          }
+        });
+        
+        
       
     }
     boatButtons[4].setIcon(ac);
     boatButtons[3].setIcon(destroyer);
     boatButtons[2].setIcon(sub);
     boatButtons[1].setIcon(patrol);
+    
+
     
     add(middlePanel, BorderLayout.CENTER);
     
